6c065d8a1f54d077f2c6cdb683d6f94676d9bdd7
damir <d.aminov@veleres.ru>
2026-02-23 21:53:11 +0500

    updated to mapped sqlalchemy 2.0

--- src/core/database/models/common.py
+++ src/core/database/models/common.py
@@ -1,7 +1,10 @@
+from typing import Optional
+from datetime import datetime
 import sqlalchemy as sa
+from sqlalchemy.orm import Mapped, mapped_column
 
 
 class BaseModel(object):
 
-    id =  sa.Column(sa.Integer, primary_key=True)
-    created_at = sa.Column(sa.DateTime, default=sa.func.now(), server_default=sa.func.now())
+    id: Mapped[int] = mapped_column(sa.Integer, primary_key=True)
+    created_at: Mapped[Optional[datetime]] = mapped_column(sa.DateTime, default=sa.func.now(), server_default=sa.func.now())
--- src/core/database/models/task.py
+++ src/core/database/models/task.py
@@ -1,5 +1,8 @@
 import sqlalchemy as sa
 from sqlalchemy.dialects.postgresql import ENUM
+from typing import Optional, Any
+from datetime import datetime
+from sqlalchemy.orm import Mapped, mapped_column
 
 from src.core.database.db import Base
 from src.core.database.models.common import BaseModel
@@ -9,11 +12,11 @@ class Task(BaseModel, Base):
 
     __tablename__ = "tasks"
 
-    name = sa.Column(sa.String, nullable=False)
-    description = sa.Column(sa.String, nullable=False)
-    priority = sa.Column("priority_type", ENUM("LOW", "MEDIUM", "HIGH",name="priority_type"), default="LOW", nullable=False)
-    status = sa.Column("task_status", ENUM("NEW", "PENDING", "IN_PROGRESS", "COMPLETED", "FAILED", "CANCELLED", name="task_status"), default="NEW", nullable=False)
-    started_at = sa.Column(sa.DateTime)
-    completed_at = sa.Column(sa.DateTime)
-    result = sa.Column(sa.JSON)
-    errors_occurred = sa.Column(sa.JSON)
+    name: Mapped[str] = mapped_column(sa.String(), nullable=False)
+    description: Mapped[str] = mapped_column(sa.String(), nullable=False)
+    priority: Mapped[str] = mapped_column(ENUM("LOW", "MEDIUM", "HIGH", name="priority_type"), default="LOW", nullable=False)
+    status: Mapped[str] = mapped_column(ENUM("NEW", "PENDING", "IN_PROGRESS", "COMPLETED", "FAILED", "CANCELLED", name="task_status"), default="NEW", nullable=False)
+    started_at: Mapped[Optional[datetime]] = mapped_column(sa.DateTime)
+    completed_at: Mapped[Optional[datetime]] = mapped_column(sa.DateTime)
+    result: Mapped[Optional[Any]] = mapped_column(sa.JSON)
+    errors_occurred: Mapped[Optional[Any]] = mapped_column(sa.JSON)
